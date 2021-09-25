012753d9171762d478f47cd7abf04df50954fe74
4restty <shota1072@gmail.com>
2021-09-25 08:56:30 +0000

    add genre list #6

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: {
     sessions: "admin/sessions"
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :genres
+  end
 end
