68ea7856026b086e70ea400e84063a24264d0296
4restty <shota1072@gmail.com>
2021-10-01 08:59:28 +0000

    add item details and item edit#15

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -16,6 +16,22 @@ class Admin::ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path
+    end
+  end
+
   private
 
   def item_params
