adecaf3d67bfe5b67adff17e3f95e07967924fcd
4restty <shota1072@gmail.com>
2021-10-11 13:10:05 +0000

    add customer details for admin#19

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,10 @@
 class OrderDetail < ApplicationRecord
+  enum order_status: {
+    not_startable: 0,
+    waiting_for_production: 1,
+    in_production: 2,
+    production_completed: 3
+  }
+  belongs_to :order
+  belongs_to :item
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
     sessions: "admin/sessions"
   }
   namespace :admin do
+    root to: "/admin/homes#top"
     resources :genres
     resources :items
     resources :customers
+    resources :orders, only:[:show, :update]
+    resources :order_details, only:[:update]
   end
 end
