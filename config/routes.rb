7ba48b706a1006ede37eae1234406052f8869b1e
4restty <shota1072@gmail.com>
2021-10-16 07:57:16 +0000

    add mypage, edit, unsubscribe#25

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :customer, module: :public
+  devise_for :customer, module: :public, only:[:registration, :session]
   devise_for :admin, controllers: {
     sessions: "admin/sessions"
-  }
+  }, skip: [:password, :registration]
 
   scope module: :public do
     root to: "homes#top"
     get "about" => "homes#about"
     resources :items, only:[:index, :show]
+    resource :customers, only:[:show, :edit, :update]
+    post "customers/unsubscribe" => "customers#unsubscribe", as: "unsubscribe_customers"
+    patch "customers/withdraw" => "customers#withdraw", as: "withdraw_customers"
+    resources :addresses
+    resources :orders
   end
 
   namespace :admin do
